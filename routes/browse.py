6aa997961ab3c033de34d73960a2ea3f2a53c1d6
thejoonshow <thejoonshow@gmail.com>
2016-03-31 08:30:30 -0400

    Worked on Browse Hotel

--- routes/browse.py
+++ routes/browse.py
@@ -5,17 +5,4 @@ app_browse = Blueprint('browse', __name__)
 
 @app_browse.route('/browse')
 def browse():
-	# locations = []
-
-	# for record in db.cities.find():
-	# 	ratings = record['ratings']
-	# 	total = 0.0
-	# 	counter = 0.0
- 		
-	# 	for x in range(0, len(ratings)):
-	# 		total += float(str(ratings[x]))
-	# 		counter += 1.0
-
-	# 	average = total/counter
-	# 	hotels.append({ "_id" : str(record['_id']) , "name" : record['name'], "rating" : round(average, 2), "url" : str(record['_id']) })
-	return render_template('browse.html', hotels=hotels)
+	return render_template('browse.html')
