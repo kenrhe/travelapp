8521a9fa86dbf47f350d50bf2efcc666300f799d
Kenneth Rhee <kennethhrhee@gmail.com>
2016-05-12 08:38:38 -0400

    remove flask

--- routes/search.py
+++ routes/search.py
@@ -12,12 +12,6 @@ def search():
         city = db.cities.find_one({"city" : form['text'].lower().strip() })
         item['city'] = city
 
-        db.runCommand({ 'getParameter' : 1, 'textSearchEnabled' : 1})
-        db.cities.ensure_index([('city', 'text'), ('state', 'text'), ('state_code', 'text')],name="default_index",weights={'city':100,'state':100, 'state_code' : 100})
-        text_results = db.command('text', db.cities.name, search=form['text'],project={"_id": 1}, limit=100)
-
-        print text_results
-
         return render_template('city.html', **item)
 
     return render_template('search.html', **item)
