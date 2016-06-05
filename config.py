a37aaa59e4378ae145a0cf63136a4ec99da3f5e4
Kenneth Rhee <kennethhrhee@gmail.com>
2016-06-05 18:21:08 -0400

    config changes for eb and city css edits

--- config.py
+++ config.py
@@ -4,39 +4,37 @@ from flask import Flask
 
 application = Flask(__name__)
 
-# try:
+try:
 application.config.from_pyfile("dev_config.cfg")
 application.config['SERVER'] = 'local server'
 mc = MongoClient( application.config['MONGODB_URI'] )
 db = mc.travelapplication
 
 print(">>> Development configuration file loaded.")
-# except:
-#   #======================================
-#   # Try to get amazon ec2 container tags
-#   #======================================
-#   pass
-#   import boto.utils
-#   import boto.ec2
+except:
+  #======================================
+  # Try to get amazon ec2 container tags
+  #======================================
+  pass
+  import boto.utils
+  import boto.ec2
 
-#   iid_doc = boto.utils.get_instance_identity()['document']
-#   region = iid_doc['region']
-#   instance_id = iid_doc['instanceId']
+  iid_doc = boto.utils.get_instance_identity()['document']
+  region = iid_doc['region']
+  instance_id = iid_doc['instanceId']
 
-#   ec2 = boto.ec2.connect_to_region(region)
-#   instance = ec2.get_only_instances(instance_ids=[instance_id])[0]
+  ec2 = boto.ec2.connect_to_region(region)
+  instance = ec2.get_only_instances(instance_ids=[instance_id])[0]
 
-#   tags = instance.tags
+  tags = instance.tags
 
-#   config = {}
+  config = {}
 
-#   config['DB_IP'] = tags['DB_IP']
-#   config['MATRIX_API_KEY'] = [ tags['MATRIX_KEY'] ]
-#   config['GEOCODER_API_KEY'] = tags['GEOCODER_KEY'].split(":")
-#   config['EMAIL'] = { 'username' : tags['EMAIL_USER'], 'password' : tags['EMAIL_PASS'] }
-#   config['DEBUG'] = tags['DEBUG'] == 'True'
-#   config['SERVER'] = tags['elasticbeanstalk:environment-name']
+  mc = MongoClient( application.config['MONGODB_URI'] )
+  db = mc.travelapplication
 
-#   application.config.update(config)
-#   print '>>> Loaded amazon ec2 container tags'
-#   print '[Current Configuration]\nDebug: %s\nServer: %s\nDB IP: %s' % (str(config['DEBUG']), config['SERVER'], config['DB_IP'])
+  config['MONGODB_URI'] = tags['MONGODB_URI']
+  config['DEBUG'] = tags['DEBUG'] == 'True'
+  application.config['SERVER'] = 'eb server'
+
+  application.config.update(config)
