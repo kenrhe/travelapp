ee4fccb5ed3d0f35260f939dd9343c113abddced
Kenneth Rhee <kennethhrhee@gmail.com>
2016-06-05 18:59:05 -0400

    Requirements.txt change

--- application.py
+++ application.py
@@ -23,6 +23,10 @@ def index():
 def cities():
     return render_template('cities.html')
 
+@application.route('/about')
+def about():
+    return render_template('about.html')
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     application.run(host='0.0.0.0', port=port)
