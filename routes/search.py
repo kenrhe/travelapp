6d9e63a470cb8eb6c313cccfb8382512b01f8d6a
Kenneth Rhee <kennethhrhee@gmail.com>
2016-06-05 22:54:25 -0400

    finishing touches

--- routes/search.py
+++ routes/search.py
@@ -12,17 +12,21 @@ def search():
     if request.method == 'POST':
         form = request.form
         search = form['text'].strip().lower().split(",")
-        if len(search) > 1:
-            print "found city + state search"
-            city = search[0].strip().replace(" ", "-")
-            state = search[1].strip().replace(" ", "-")
-            print city
-            print state
-            city_data = db.cities.find_one({"city" : city, "state" : state})
-        else:
-            city_data = db.cities.find_one({"city" : form['text'].lower().strip().replace(" ", "-") })
-        
+        try:
+            if len(search) > 1:
+                print "found city + state search"
+                city = search[0].strip().replace(" ", "-")
+                state = search[1].strip().replace(" ", "-")
+                print city
+                print state
+                city_data = db.cities.find_one({"city" : city, "state" : state})
+            else:
+                city_data = db.cities.find_one({"city" : form['text'].lower().strip().replace(" ", "-") })
 
-        return redirect('/' + city_data['_id'])
+            return redirect('/' + city_data['_id'])
+        except TypeError:
+            item['error'] = 'Hey, we were unable to find that city.. please contact us for more assistnace.'
+
+        
 
     return render_template('search.html', **item)
